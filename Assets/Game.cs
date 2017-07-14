3edf5d6fa0557a3e6da10188db6d8e20b31b42c1
fperret <fperret@student.42.fr>
2017-07-14 17:11:40 +0200

    beginning

---
+++ Assets/Game.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    public const int gridLength = 20;
+    public const int gridCapacity = gridLength * gridLength * gridLength;
+
+    private TestUnit testUnit;
+
+    public GameObject cubePrefab;
+
+    public GameObject[,,] goGrid = new GameObject[gridLength, gridLength, gridLength];
+    public bool[,,] grid = new bool[gridLength, gridLength, gridLength];
+
+    private void Awake()
+    {
+
+        for (int z = 0; z < gridLength; ++z)
+        {
+            for (int y = 0; y < gridLength; ++y)
+            {
+                for (int x = 0; x < gridLength; ++x)
+                {
+                    goGrid[z, y, x] = Instantiate(cubePrefab, new Vector3(x, y, z), Quaternion.identity);
+                }
+            }
+        }
+    }
+
+    private void changeCubeColorAtIndex(int x, int y, int z)
+    {
+        goGrid[z, y, x].GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    private IEnumerator magic()
+    {
+        for (int z = 0; z < gridLength; ++z)
+        {
+            for (int y = 0; y < gridLength; ++y)
+            {
+                for (int x = 0; x < gridLength; ++x)
+                {
+                    changeCubeColorAtIndex(x, y, z);
+                    yield return 0;
+                }
+            }
+        }
+    }
+
+    private int countFront(int x, int y, int z)
+    {
+        int count = 0;
+        count += this.grid[z - 1, y - 1, x - 1] ? 1 : 0;
+        count += this.grid[z - 1, y - 1, x] ? 1 : 0;
+        count += this.grid[z - 1, y - 1, x + 1] ? 1 : 0;
+        count += this.grid[z - 1 , y, x - 1] ? 1 : 0;
+        count += this.grid[z - 1, y, x] ? 1 : 0;
+        count += this.grid[z - 1, y, x + 1] ? 1 : 0;
+        count += this.grid[z - 1, y + 1, x - 1] ? 1 : 0;
+        count += this.grid[z - 1, y + 1, x] ? 1 : 0;
+        count += this.grid[z - 1, y + 1, x + 1] ? 1 : 0;
+        return (count);
+    }
+
+    public int countNeighborsAtIndex(int x, int y, int z)
+    {
+        int count = 0;
+        count += countFront(x, y, z);
+        return (count);
+    }
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(magic());
+        this.testUnit = new TestUnit(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/TestUnit.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TestUnit {
+
+    private Game game;
+
+    public TestUnit(Game game)
+    {
+        this.game = game;
+        this.initGrid();
+        runAllTests();
+    }
+
+    private void initGrid()
+    {
+        for (int z = 0; z < Game.gridLength; ++z)
+        {
+            for (int y = 0; y < Game.gridLength; ++y)
+            {
+                for (int x = 0; x < Game.gridLength; ++x)
+                {
+                    game.grid[z, y, x] = false;
+                }
+            }
+        }
+    }
+
+    private bool centerNoNeighbor()
+    {
+        this.initGrid();
+        if (this.game.countNeighborsAtIndex(5, 5, 5) != 0)
+        {
+            return (false);
+        } else
+        {
+            return (true);
+        }
+    }
+
+    private bool centerFullFrontNeighbor()
+    {
+        this.initGrid();
+        for (int y = 4; y < 7; ++y)
+        {
+            for (int x = 4; x < 7; ++x)
+            {
+                this.game.grid[4, y, x] = true;
+            }
+        }
+        this.game.grid[5, 5, 5] = true;
+        if (this.game.countNeighborsAtIndex(5, 5, 5) != 9)
+        {
+            return (false);
+        } else
+        {
+            return (true);
+        }
+    }
+
+    private void runAllTests()
+    {
+        try
+        {
+            if (!this.centerNoNeighbor())
+            {
+                throw new System.Exception();
+            }
+            if (!this.centerFullFrontNeighbor())
+            {
+                throw new System.Exception();
+            }
+        } catch (System.Exception e)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+            Debug.Log("fjwefjwe");
+        }
+
+    }
+
+}
